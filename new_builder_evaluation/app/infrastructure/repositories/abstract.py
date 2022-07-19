729fc68aea619ea58d8d78c70c7acb71dd846b42
remg1997 <rafael.mosquera@factored.ai>
2022-07-19 15:18:56 -0500

    Change abstract for linter test

--- new_builder_evaluation/app/infrastructure/repositories/abstract.py
+++ new_builder_evaluation/app/infrastructure/repositories/abstract.py
@@ -1,5 +1,6 @@
-from app.infrastructure.connection import  Connection
 from app.infrastructure.utils.instance_converter import ORMInstanceConverter
+from app.infrastructure.connection import  Connection
+
 
 
 class AbstractRepository:
@@ -9,18 +10,18 @@ class AbstractRepository:
         self.model = model
         self.instance_converter = ORMInstanceConverter()
         self.rollback = self.session.rollback()
-        
+
     def get_all(self):
         instances = self.session.query(self.model).all()
         return instances
-    
+
     def add(self, instance_dict: dict):
         new_instance = self.model(**instance_dict)
         self.session.add(new_instance)
         self.session.commit()
         new_instance = self.instance_converter.instance_to_dict(new_instance)
         return new_instance
-    
+
     def get_by_id(self, id: int) -> dict:
         instance = self.session.query(self.model).get(id)
         instance = self.instance_converter.instance_to_dict(instance)
