0d0b9f68ea4cc56e0b8799b60e2db4a66bd910fd
remg1997 <rafael.mosquera@factored.ai>
2022-07-19 18:00:37 -0500

    Include per-tag evaluation

--- new_builder_evaluation/app/domain/eval_utils/evaluator.py
+++ new_builder_evaluation/app/domain/eval_utils/evaluator.py
@@ -1,28 +1,36 @@
 from .metrics_dicts import eval_metrics_dict, delta_metrics_dict
 
+from metrics_dicts import eval_metrics_dict, delta_metrics_dict
+
 class Evaluator:
     def __init__(self, config: dict):
         self.config = config
         self.metric = config['perf_metric']['type']
         self.metric_func = eval_metrics_dict[self.metric]
         self.perturb_prefixes = [metric['type'] for metric in config['delta_metrics']]
-    
+
     def evaluate(self, formatted_predictions: list, formatted_labels: list) -> dict:
-        """ 
-        Evaluates a list of predictions against a list of labels 
-        using a metric included in the metrics_dictionary. 
+        """
+        Evaluates a list of predictions against a list of labels
+        using a metric included in the metrics_dictionary.
         """
         target_ids = [x["id"] for x in formatted_labels]
-        target_labels_dict = {t["id"]: t["answer"] for t in formatted_labels}
-        target_labels = [target_labels_dict[id] for id in target_ids]
+        target_labels_dict = {t["id"]: t["answer"] for t in formatted_labels} # labels
+        target_labels = [target_labels_dict[ids] for ids in target_ids]
         target_tags_dict = {t["id"]: t["tags"] for t in formatted_labels}
-        target_tags = [target_tags_dict[id] for id in target_ids]
+        target_tags = [target_tags_dict[ids] for ids in target_ids]
 
-        prediction_labels = {p["id"]: p["pred"] for p in formatted_predictions}
-        prediction_labels = [prediction_labels[id] for id in target_ids]
+        prediction_labels_dict = {p["id"]: p["pred"] for p in formatted_predictions} # predictions
+        prediction_labels = [prediction_labels_dict[ids] for ids in target_ids]
         assert len(prediction_labels) == len(target_labels)
 
-        perf, perf_dict = self._compute_metric(prediction_labels, target_labels)
+        predictions=[]
+        labels=[]
+        for ids, target_label in target_labels_dict.items():
+            predictions.append(prediction_labels_dict[ids])
+            labels.append(target_label)
+
+        perf, perf_dict = self._compute_metric(predictions, labels)
 
         score_obj = {}
         score_obj["perf"] = perf
@@ -30,36 +38,63 @@ class Evaluator:
         score_obj["pretty_perf"] = str(perf) + " %"
         score_obj["metadata_json"] = perf_dict
 
+        if target_tags:
+
+            examples_by_tag = {}
+            for ids, pred in prediction_labels_dict.items():
+                tags = target_tags_dict[ids]
+                target_label = target_labels_dict[ids]
+                for tag in tags:
+                    examples_by_tag.setdefault(tag, []).append((ids, pred, target_label))
+
+            perf_by_tag_tuple_dict ={}
+            for k, examples in examples_by_tag.items():
+                ids, pred, target_label = list(zip(*examples))
+                perf_by_tag_tuple_dict[k]=self._compute_metric(pred, target_label)
+
+            score_obj["metadata_json"]["perf_by_tag"] = score_obj[
+                "metadata_json"
+            ].get("perf_by_tag", []) + [
+                {
+                    "tag": tag,
+                    "pretty_perf": str(perf) + " %",
+                    "perf": perf,
+                    "perf_std": perf_dict.get("perf_std", None),
+                    "perf_dict": perf_dict,
+                }
+                for tag, (perf, perf_dict) in perf_by_tag_tuple_dict.items()
+            ]
+
         return score_obj
-    
+
     def evaluate_delta_metrics(self,
                                grouped_predictions: list,
                                grouped_robusts: list,
                                grouped_fairs: list) -> list:
-        
+
         """
         Calculates the delta metric given a perturb prefix,
-        comparing original predictions with robust and fair 
+        comparing original predictions with robust and fair
         predictions  #Strong assumption is not having to
         calculate alternative metrics from the yaml one.
         """
-        delta_metrics = {}
-        
+        delta_metrics = []
+
         for prefix in self.perturb_prefixes:
             if prefix == 'robustness':
                 delta_metric = self._compute_delta_metrics(grouped_robusts,
                                                            grouped_predictions,
                                                            prefix)
-                delta_metrics['robustness'] = delta_metric.get('robustness')
+                delta_metrics.append(delta_metric)
             else:
                 delta_metric = self._compute_delta_metrics(grouped_fairs,
                                                            grouped_predictions,
                                                            prefix)
-                delta_metrics['fairness'] = delta_metric.get('fairness')
-        
+                delta_metrics.append(delta_metric)
+
         return delta_metrics
-        
-        
+
+
     def _compute_metric(self, predictions: list, targets: list) -> tuple:
         metric_result = self.metric_func(predictions, targets)
         if isinstance(metric_result, dict):
@@ -67,14 +102,14 @@ class Evaluator:
         else:
             score_dict = {self.metric: metric_result}
         return score_dict[self.metric], score_dict
-    
-    def _compute_delta_metrics(self, 
+
+    def _compute_delta_metrics(self,
                                grouped_predictions: list,
                                grouped_labels: list,
                                perturb_prefix: str)-> dict:
         """
         predictions: a list of list of predictions. If computing robustness, predictions must
-        be concatenated by id.  
+        be concatenated by id.
         targets: a list of labels
         """
         perf_metric = eval_metrics_dict[self.metric]
