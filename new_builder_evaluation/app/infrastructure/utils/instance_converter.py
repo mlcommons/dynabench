d1c1ebc1a88a4b662b002d4fdcb7b3e66e03db15
remg1997 <rafael.mosquera@factored.ai>
2022-07-19 14:00:16 -0500

    Include ORM utilities

---
+++ new_builder_evaluation/app/infrastructure/utils/instance_converter.py
@@ -0,0 +1,28 @@
+from app.infrastructure.models.models import Base
+
+class ORMInstanceConverter:
+    def instance_to_dict(self, instance):
+        """
+        Converts an ORM instance or list of instances into a dictionary
+        or list of dictionaries respectively.
+        """
+        if isinstance(instance, (list, tuple)):
+            return self._instance_list_to_dict(instance)
+        return self._single_instance_to_dict(instance)
+
+    def _single_instance_to_dict(self, instance):
+        # If instance is not an ORM entity it must be
+        # a string, integer or a normal data type
+        # hence we do not have to convert it further
+        if not isinstance(instance, Base):
+            return instance
+        dictionary = {
+            column: self.instance_to_dict(value)
+            for column, value in instance.__dict__.items()
+            if not column.startswith("_")  # Filter out internal use attributes
+        }
+        return dictionary
+
+    def _instance_list_to_dict(self, instances: list):
+        dict_rows = [self.instance_to_dict(instance) for instance in instances]
+        return 
