23fc8312a587b862cdf7ef316adabfe19692f7b5
remg1997 <rafael.mosquera@factored.ai>
2022-07-19 14:02:41 -0500

    Upload the input formatter for evaluation

---
+++ new_builder_evaluation/app/domain/eval_utils/input_formatter.py
@@ -0,0 +1,78 @@
+from collections import defaultdict
+class InputFormatter:
+    def __init__(self, config: dict):
+        self.config = config
+        self.label = config['perf_metric']['reference_name']
+        
+    def format_labels(self, examples: list) -> list:
+        """
+        Convert the labels to a format expected by Evaluator.
+        Can format original labels, as well as robustness and fairness labels.
+        {
+            "id": <a unique identifier>
+            "answer": <the correct label for a given sample>
+            "tags": <a list of strings> 
+        }
+        """
+        formatted_examples = []
+        
+        for example in examples:
+            formatted_example = {
+                "id": example["uid"],
+                "answer": example[self.label],
+                "tags": example.get("tags", []),
+            }
+            formatted_examples.append(formatted_example)
+        
+        return formatted_examples
+            
+
+    def format_predictions(self, examples: list) -> list:
+        """
+        Convert the prediction to a format expected by Evaluator.
+        Can format original predictions, as well as robustness and fairness predictions.
+        {
+            "id": <a unique identifier>,
+            "pred": <the prediction that will be used to calculate metrics>,
+        }
+        """
+        formatted_examples = []
+        
+        for example in examples:
+            formatted_example = {
+                "id": example["id"],
+                "pred": example[self.label],
+            }
+            formatted_examples.append(formatted_example)
+        
+        return formatted_examples
+    
+    @staticmethod
+    def group_predictions(examples: list) -> dict:
+        """
+        Group predictions with same ID from robustness inference,
+        to compute delta metrics.
+        
+        """
+        grouped_predictions = defaultdict(list)
+        
+        for example in examples:
+            id = example['id'].split('_')[0]
+            grouped_predictions[id].append(example['pred'])
+            
+        return grouped_predictions
+    
+    @staticmethod
+    def group_labels(examples: list) -> dict:
+        """
+        Group labels with same ID from robustness inference,
+        to compute delta metrics.
+        
+        """
+        final_labels = defaultdict(list)
+        
+        for example in examples:
+            id = example['id'].split('_')[0]
+            final_labels[id] = (example['answer'])
+            
+        return final_labels
