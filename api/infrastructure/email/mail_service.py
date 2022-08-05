5b9e44c9e77bf50a23ab69ab9c96c551cebb8d8f
Julian Eusse <jeussej@gmail.com>
2022-08-05 12:39:02 -0500

    add mail and pasword to env

--- api/infrastructure/email/mail_service.py
+++ api/infrastructure/email/mail_service.py
@@ -2,15 +2,23 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import os
 import pathlib
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from string import Template
 
+from dotenv import load_dotenv
+
 
 class Email:
     def __init__(self):
+        load_dotenv()
+
+        self.login = os.getenv("MAIL_LOGIN")
+        self.pwd = os.getenv("MAIL_PASWORD")
+
         self.local_dir = pathlib.Path(__file__).parent
 
     def _read_template(self, filename):
@@ -27,14 +35,12 @@ class Email:
         return Template(template_file_content)
 
     def send(self, contact, cc_contact=None, template_name="", msg_dict={}, subject=""):
-        login = "dynabench-site@mlcommons.org"
-        pwd = "pcobtnfznqmlinpl"
 
         try:
             msg = MIMEMultipart()
             message_template = self._read_template(template_name)
             message = message_template.substitute(msg_dict)
-            msg["From"] = login
+            msg["From"] = self.login
             msg["To"] = contact
             if cc_contact is not None:
                 msg["Cc"] = cc_contact
@@ -44,8 +50,8 @@ class Email:
             server = smtplib.SMTP("smtp.gmail.com", 587)
             server.ehlo()
             server.starttls()
-            server.login(login, pwd)
-            server.sendmail(login, contact, msg.as_string())
+            server.login(self.login, self.pwd)
+            server.sendmail(self.login, contact, msg.as_string())
             server.close()
             print(f"e-mail sended to {contact}")
             return True
@@ -53,20 +59,3 @@ class Email:
         except Exception:
             print("Error sending e-mail")
             return False
-
-
-def send_email(subject, body, send_to):
-    login = "dynabench-site@mlcommons.org"
-    pwd = "pcobtnfznqmlinpl"
-    msg = MIMEMultipart()
-    msg["From"] = login
-    msg["To"] = send_to
-    msg.set_charset("utf-8")
-    msg["Subject"] = subject
-    msg.attach(MIMEText(body, "plain"))
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.ehlo()
-    server.starttls()
-    server.login(login, pwd)
-    server.sendmail(login, send_to, msg.as_string())
-    server.close()
