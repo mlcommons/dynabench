0fa0c502dca7e436e6788dccf40c450752a23137
remg1997 <rafael.mosquera@factored.ai>
2023-02-21 11:52:30 -0500

    Adapt possible context getters

--- backend/app/infrastructure/repositories/context.py
+++ backend/app/infrastructure/repositories/context.py
@@ -5,8 +5,9 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+from sqlalchemy import case, func
 
-from app.infrastructure.models.models import Context
+from app.infrastructure.models.models import Context, Example
 from app.infrastructure.repositories.abstract import AbstractRepository
 
 
@@ -27,3 +28,44 @@ class ContextRepository(AbstractRepository):
             .first()
         )
         return instance.r_realid
+
+    def get_random(self, real_round_id: int, n: int = 1, tags=None):
+        instance = (
+            self.session.query(self.model)
+            .filter(self.model.r_realid == real_round_id)
+            .order_by(func.rand())
+            .limit(n)
+            .all()
+        )
+        return instance
+
+    def get_least_used(self, real_round_id: int, n: int = 1, tags=None):
+        instance = (
+            self.session.query(self.model)
+            .filter(self.model.r_realid == real_round_id)
+            .order_by(self.model.total_used.asc(), func.rand())
+            .limit(n)
+            .all()
+        )
+        return instance
+
+    def get_least_fooled(self, real_round_id: int, n: int = 1, tags=None):
+        query = (
+            self.session.query(self.model)
+            .join(Example, self.model.id == Example.cid, isouter=True)
+            .filter(self.model.r_realid == real_round_id)
+        )
+
+        query = (
+            query.group_by(self.model.id)
+            .order_by(
+                func.sum(case(value=Example.model_wrong, whens={1: 1}, else_=0)),
+                func.rand(),
+            )
+            .with_entities(self.model)
+        )
+
+        return query.limit(n).all()
+
+    def get_validation_failed(self, real_round_id: int, n: int = 1, tags=None):
+        return None
