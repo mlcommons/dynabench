13f49a11da45046362e6aca09e3cda69038a40c5
Juan Manuel Ciro Torres <jmcirot@unal.edu.co>
2023-04-10 23:41:54 -0500

    react dom

--- frontends/web/mturk-src/index.js
+++ frontends/web/mturk-src/index.js
@@ -4,24 +4,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-/*
- * Copyright (c) Facebook, Inc. and its affiliates.
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import React from "react";
-import * as ReactDOMClient from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App.js";
+import App from "./containers/App";
 
-const container = document.getElementById("app");
-
-// Create a root.
-const root = ReactDOMClient.createRoot(container);
-
-// Initial render: Render an element to the root.
-root.render(<App tab="home" />);
-
-// During an update, there's no need to pass the container again.
-root.render(<App tab="profile" />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
