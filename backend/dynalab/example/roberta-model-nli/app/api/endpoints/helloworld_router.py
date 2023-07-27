17f40fbb6f23e12f4a93de26c4ee3ab196c4a45d
Juan Manuel Ciro Torres <jmcirot@unal.edu.co>
2023-07-26 20:26:49 -0500

    just moving folders

---
+++ backend/dynalab/example/roberta-model-nli/app/api/endpoints/helloworld_router.py
@@ -0,0 +1,17 @@
+# Copyright (c) MLCommons and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from fastapi import APIRouter
+
+
+router = APIRouter()
+
+
+@router.get("/")
+async def hello():
+    return {"message": "Hello World"}
