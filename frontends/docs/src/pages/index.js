dbd817b22b376d4ecd1117d37a0fce87130c9d19
Juan Manuel Ciro Torres <jmcirot@unal.edu.co>
2023-08-14 20:34:01 -0500

    add page for documentation

---
+++ frontends/docs/src/pages/index.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) MLCommons and its affiliates.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <main></main>
+    </Layout>
+  );
+}
---
+++ frontends/docs/src/theme/NavbarItem/ComponentTypes.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) MLCommons and its affiliates.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import ComponentTypes from "@theme-original/NavbarItem/ComponentTypes";
+import SecondLogo from "@site/src/components/NavbarItems/SecondLogo";
+import Communities from "@site/src/components/NavbarItems/Communities";
+import About from "@site/src/components/NavbarItems/About";
+
+export default {
+  ...ComponentTypes,
+  "custom-secondLogo": SecondLogo,
+  "custom-communities": Communities,
+  "custom-about": About,
+};
