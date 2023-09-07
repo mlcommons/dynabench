7467c991b64576c55a458084115f194d2acf0809
Juan Ciro <juanciro@Juans-MacBook-Pro.local>
2023-09-07 13:06:07 -0500

    remove SD model

--- backend/app/domain/services/utils/multi_generator.py
+++ backend/app/domain/services/utils/multi_generator.py
@@ -7,7 +7,6 @@ from multiprocessing import Pool
 from app.domain.services.utils.image_generators import (  # DynabenchImageProvider,
     HFImageProvider,
     OpenAIImageProvider,
-    SDVariableAutoEncoder,
 )
 
 
@@ -16,7 +15,7 @@ class ImageGenerator:
         self.image_providers = [
             HFImageProvider(),
             # DynabenchImageProvider(),
-            SDVariableAutoEncoder(),
+            # SDVariableAutoEncoder(),
             OpenAIImageProvider(),
         ]
 
