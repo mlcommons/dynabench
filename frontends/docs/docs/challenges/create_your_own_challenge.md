44d8c8c43bfeba4aa777e21012f57a84e96adaf4
Juan Manuel Ciro Torres <jmcirot@unal.edu.co>
2023-12-06 09:40:34 -0500

    Add page for documentation (#194)
    * add page for documentation
    
    * feat: add mlcommons page
    
    * feat: update readme
    
    * feat:new docs
    
    * feat: add new docs
    
    * feat: add new docs
    
    * bug: solve problem with is admin func

---
+++ frontends/docs/docs/challenges/create_your_own_challenge.md
@@ -0,0 +1,29 @@
+---
+sidebar_position: 1
+---
+
+# Creating Your Own Challenge on Dynabench
+
+Welcome to the World of AI Challenges! Dynabench is an open-source platform supported by MLCommons, and we're always excited to hear fresh ideas for new challenges. If you have a unique challenge in mind, you can create it within our platform. Here's how:
+
+1. **Get Started**
+   Visit Dynabench and create your user account. If you already have an account, simply log in.
+
+2. **Let's Create a Challenge!**
+   Once you're on the homepage, click on the "Create Your Own Challenge" button.
+
+3. **Define Your Challenge**
+   Provide the following information:
+
+   - **Challenge Name:** Choose a catchy name that represents your challenge.
+   - **Unique Challenge Code:** Create a unique code (you can use the name plus a number) for your challenge.
+   - **Short Challenge Description:** Write a brief and compelling description of your challenge.
+
+4. **Submit Your Proposal**
+   After this, you'll receive an email confirming that your challenge proposal has been received. Within a few days, a member of the Dynabench team will reach out to you to discuss your idea further.
+
+## Join the AI Challenge Revolution!
+
+We can't wait to see what incredible challenges you'll bring to Dynabench. Let's push the boundaries of AI together!
+
+If you have any questions or need assistance, feel free to reach out. We're here to help you every step of the way.
---
+++ frontends/docs/docusaurus.config.js
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) MLCommons and its affiliates.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Dynabench",
+  tagline: "A platform for dynamic benchmarking of AI models",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://dynabench.org",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "MLCommons", // Usually your GitHub org/user name.
+  projectName: "dynabench", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/mlcommons/dynabench/edit/main/frontends/docs/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: "light",
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "YOUR_APP_ID",
+
+        // Public API key: it is safe to commit it
+        apiKey: "YOUR_SEARCH_API_KEY",
+
+        indexName: "YOUR_INDEX_NAME",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: "external\\.com|domain\\.com",
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: "/docs/", // or as RegExp: /\/docs\//
+          to: "/",
+        },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
+
+        //... other Algolia params
+      },
+      // Replace with your project's social card
+      image: "img/logo_black.png",
+      navbar: {
+        logo: {
+          alt: "Dynabench",
+          src: "img/logo_mlcommos_white.png",
+          href: "https://dynabench.org",
+        },
+        items: [
+          {
+            type: "custom-secondLogo",
+            position: "left",
+          },
+          {
+            type: "custom-about",
+            position: "left",
+          },
+          {
+            type: "custom-communities",
+            position: "left",
+          },
+          {
+            href: "https://github.com/mlcommons/dynabench",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        copyright: `Copyright © ${new Date().getFullYear()} MLCommons, Inc.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+  plugins: [
+    async function myPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
+  ],
+};
+
+module.exports = config;
