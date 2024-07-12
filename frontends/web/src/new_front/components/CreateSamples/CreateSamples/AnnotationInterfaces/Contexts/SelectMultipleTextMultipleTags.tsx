e02a49d19170b6e9a935ffba2d709209b1a41adc
Sara H <shincap8@gmail.com>
2024-07-12 18:21:41 -0500

    Multiple colors and tags working allong with dropdown search

--- frontends/web/src/new_front/components/CreateSamples/CreateSamples/AnnotationInterfaces/Contexts/SelectMultipleTextMultipleTags.tsx
+++ frontends/web/src/new_front/components/CreateSamples/CreateSamples/AnnotationInterfaces/Contexts/SelectMultipleTextMultipleTags.tsx
@@ -4,11 +4,61 @@ import { ContextConfigType } from "new_front/types/createSamples/createSamples/a
 import { ContextAnnotationFactoryType } from "new_front/types/createSamples/createSamples/annotationFactory";
 import React, { FC, useEffect, useState, useContext } from "react";
 import { TokenAnnotator } from "react-text-annotate";
+import DropdownSearch from "new_front/components/Inputs/DropdownSearch";
+
+import languages from "./BCP47Temp";
+
+type MultipleTagsTypes = {
+  preselectedTag?: string;
+};
+
+const text =
+  'On 31 January 2014, it was reported that Neesonwould workwith director Martin Scorsese again in an adaptation of the novel "Silence". Neeson had a supporting role as the henchman Bad Cop/Good Cop in the animated film "The Lego Movie", which was a critical and commercial success. Neeson later played Bill Marks in the 2014 action film Non-Stop. The film was releasedon 28 February 2014. He also appeared, uncredited, as God in the BBC2 series "Rev.". Neeson stars in the 2014 film "A Walk Among the Tombstones", an adaption of the best-selling novel of the same name, in which he plays former cop Matthew Scudder, a detective hired to hunt the killers of a drug dealer\'s wife.';
 
 const SelectMultipleTextMultipleTags: FC<
-  ContextAnnotationFactoryType & ContextConfigType
-> = ({ field_names_for_the_model, context, instruction, metadata }) => {
+  ContextAnnotationFactoryType & ContextConfigType & MultipleTagsTypes
+> = ({
+  field_names_for_the_model,
+  context,
+  instruction,
+  metadata,
+  preselectedTag,
+}) => {
   const { updateModelInputs } = useContext(CreateInterfaceContext);
+  const [selectionInfo, setSelectionInfo] = useState<any>([]);
+  const [tags, setTags] = useState<any>([]);
+  const [tagSelection, setTagSelection] = useState<any>(preselectedTag || null);
+  const [tagColors, setTagColors] = useState<any>(undefined);
+
+  const generateRandomColor = (): string => {
+    const getRelativeLuminance = (r: number, g: number, b: number): number => {
+      const rs = r / 255;
+      const gs = g / 255;
+      const bs = b / 255;
+
+      const rL =
+        rs <= 0.03928 ? rs / 12.92 : Math.pow((rs + 0.055) / 1.055, 2.4);
+      const gL =
+        gs <= 0.03928 ? gs / 12.92 : Math.pow((gs + 0.055) / 1.055, 2.4);
+      const bL =
+        bs <= 0.03928 ? bs / 12.92 : Math.pow((bs + 0.055) / 1.055, 2.4);
+
+      return 0.2126 * rL + 0.7152 * gL + 0.0722 * bL;
+    };
+    let color;
+    let luminance;
+
+    do {
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+
+      color = `rgb(${r}, ${g}, ${b})`;
+      luminance = getRelativeLuminance(r, g, b);
+    } while (luminance < 0.7); // Adjust the threshold as needed
+
+    return color;
+  };
 
   useEffect(() => {
     updateModelInputs(
@@ -17,13 +67,28 @@ const SelectMultipleTextMultipleTags: FC<
       },
       metadata.context
     );
-  }, []);
 
-  const [selectionInfo, setSelectionInfo] = useState([] as any);
+    const tempTags: any[] = [];
+    const colors: string[] = [];
+    const tempTagColors: any = {};
+    languages.forEach((tag: string) => {
+      let color = generateRandomColor();
+      while (colors.includes(color)) {
+        color = generateRandomColor();
+      }
+      colors.push(color);
+      tempTags.push({ value: tag, color: color });
+      tempTagColors[tag] = color;
+    });
+    setTagSelection(tempTags.find((tag) => tag.value === preselectedTag));
+    setTags(tempTags);
+    setTagColors(tempTagColors);
+  }, []);
 
   const handleChange = (value: any) => {
     setSelectionInfo(value);
-    updateModelInputs(
+    console.log("seleccion", value);
+    /* updateModelInputs(
       {
         [field_names_for_the_model.answer ?? "selectable_text"]: context.context
           .split(" ")
@@ -31,22 +96,81 @@ const SelectMultipleTextMultipleTags: FC<
           .join(" "),
       },
       metadata.answer
-    );
+    ); */
   };
 
   return (
     <AnnotationInstruction
       placement="top"
-      tooltip={instruction?.context || "Select one of the options below"}
+      tooltip={
+        instruction?.context ||
+        "Select the tag and the text according to the tag"
+      }
     >
-      <div className="p-2 rounded">
-        <h1>Esto si se está mostrando</h1>
-        <TokenAnnotator
-          tokens={context.context.split(" ")}
-          value={selectionInfo}
-          onChange={(value) => handleChange(value)}
-        />
-      </div>
+      <>
+        <div className="p-2 rounded">
+          <TokenAnnotator
+            tokens={text.split(" ")}
+            value={selectionInfo}
+            onChange={(value) => handleChange(value)}
+            getSpan={(span) => ({
+              ...span,
+              tag: tagSelection,
+              color: tagColors[tagSelection],
+            })}
+            renderMark={(props) => (
+              <mark
+                key={props.key}
+                onClick={() =>
+                  props.onClick({
+                    start: props.start,
+                    end: props.end,
+                    tag: props.tag,
+                    color: props.color,
+                    content: props.content,
+                  })
+                }
+                style={{
+                  padding: ".2em .3em",
+                  margin: "0 .25em",
+                  lineHeight: "1",
+                  display: "inline-block",
+                  borderRadius: ".25em",
+                  background: tagColors[props.tag],
+                }}
+              >
+                {props.content}{" "}
+                <span
+                  style={{
+                    boxSizing: "border-box",
+                    content: "attr(data-entity)",
+                    fontSize: ".55em",
+                    lineHeight: "1",
+                    padding: ".35em .35em",
+                    borderRadius: ".35em",
+                    textTransform: "uppercase",
+                    display: "inline-block",
+                    verticalAlign: "middle",
+                    margin: "0 0 .15rem .5rem",
+                    background: "#fff",
+                    fontWeight: "700",
+                  }}
+                >
+                  {" "}
+                  {props.tag}
+                </span>
+              </mark>
+            )}
+          />
+        </div>
+        <div>
+          <DropdownSearch
+            options={tags}
+            value={tagSelection || "Select a tag"}
+            onChange={setTagSelection}
+          />
+        </div>
+      </>
     </AnnotationInstruction>
   );
 };
---
+++ frontends/web/src/new_front/components/Inputs/DropdownSearch.tsx
@@ -0,0 +1,92 @@
+import React, { FC, useState } from "react";
+
+type DropdownSearchProps = {
+  options: any[];
+  value: string;
+  onChange: (prompt: any) => void;
+  disabled?: boolean;
+};
+
+const DropdownSearch: FC<DropdownSearchProps> = ({
+  options,
+  value,
+  onChange,
+  disabled = false,
+}) => {
+  const [open, setOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredOptions = options.filter((option) =>
+    option.value.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleOnClick = (option: any) => {
+    setOpen(false);
+    setSearchTerm("");
+    onChange(option.value);
+  };
+
+  return (
+    <div className="w-full pb-6 text-right ">
+      <div>
+        <button
+          type="button"
+          className=" h-[47px] inline-flex w-full gap-x-1.5 rounded-md bg-white px-3 py-2  text-letter-color shadow-md ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-none"
+          id="menu-button"
+          aria-expanded="true"
+          aria-haspopup="true"
+          onClick={() => setOpen(!open)}
+        >
+          <div className="flex justify-between w-full">
+            <span className="pt-1">{value}</span>
+            <svg
+              className="flex mt-1 -mr-1 text-gray-800 h-7 w-7 "
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+        </button>
+      </div>
+
+      <div
+        className={`${
+          open ? "block" : "hidden"
+        } right-0 mt-2 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5`}
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="menu-button"
+      >
+        <div className="py-1" role="none">
+          <input
+            type="text"
+            className="w-full p-2 border border-gray-300 rounded"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          {filteredOptions.map((option, key) => (
+            <button
+              key={key}
+              type="button"
+              className="block w-full px-4 py-2 text-left text-gray-700 border-solid rounded-sm focus:outline-none hover:bg-gray-100 hover:text-gray-900"
+              role="menuitem"
+              onClick={() => handleOnClick(option)}
+              disabled={disabled}
+            >
+              {option.value}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DropdownSearch;
