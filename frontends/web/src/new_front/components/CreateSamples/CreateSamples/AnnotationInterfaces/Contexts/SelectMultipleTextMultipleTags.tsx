365f6ad15479c935dc1de0faa996ea9e1af16baa
Sara H <shincap8@gmail.com>
2024-08-15 18:17:03 -0500

    Run pre-commit

--- frontends/web/src/new_front/components/CreateSamples/CreateSamples/AnnotationInterfaces/Contexts/SelectMultipleTextMultipleTags.tsx
+++ frontends/web/src/new_front/components/CreateSamples/CreateSamples/AnnotationInterfaces/Contexts/SelectMultipleTextMultipleTags.tsx
@@ -22,7 +22,7 @@ type Dictionary = { [key: string]: any };
 
 const cleanUpSelection = (
   selection: Array<Dictionary>,
-  keyToRemove: string
+  keyToRemove: string,
 ) => {
   const result: Array<Record<string, any>> = [];
 
@@ -93,7 +93,7 @@ const SelectMultipleTextMultipleTags: FC<
     if (preferedTag) {
       setTagSelection(localTags.find((tag: any) => tag.value === preferedTag));
       handleSubmit(
-        localTags.find((tag: any) => tag.value === preferedTag)?.back_label
+        localTags.find((tag: any) => tag.value === preferedTag)?.back_label,
       );
     }
   }, [preferedTag]);
@@ -118,7 +118,7 @@ const SelectMultipleTextMultipleTags: FC<
           key_name: field_names_for_the_model?.tag_name_search,
           key_value: value,
         }),
-      }
+      },
     )
       .then((response) => response.json())
       .then((data) => {
@@ -192,11 +192,11 @@ const SelectMultipleTextMultipleTags: FC<
     }
     const start: number = Math.min(
       value[valueLength - 1].start,
-      value[valueLength - 1].end
+      value[valueLength - 1].end,
     );
     const end: number = Math.max(
       value[valueLength - 1].start,
-      value[valueLength - 1].end
+      value[valueLength - 1].end,
     );
     valueLength > 0 && console.log("last value", value[valueLength - 1].start);
     if (
@@ -211,7 +211,7 @@ const SelectMultipleTextMultipleTags: FC<
     const already = selectionInfo.find(
       (val: any) =>
         (val.start <= start && val.end >= start) ||
-        (val.start <= end && val.end >= start)
+        (val.start <= end && val.end >= start),
     );
     value[valueLength - 1].start = start;
     value[valueLength - 1].end = end;
