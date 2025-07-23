e608ebe0e04109d041e612d7f0a5f2e71d3b74bb
Sara H <shincap8@gmail.com>
2025-07-22 22:52:48 -0500

    run prettier

--- frontends/web/src/containers/App.js
+++ frontends/web/src/containers/App.js
@@ -98,7 +98,7 @@ class RouterMonitor extends React.Component {
       ) {
         this.props.history.push(
           "/login?msg=" +
-            encodeURIComponent("You need to be logged in to access this beta."),
+            encodeURIComponent("You need to be logged in to access this beta.")
         );
       }
     }
@@ -138,7 +138,7 @@ class App extends React.Component {
               <Redirect push to="/logout" />;
               //In Case Redirect doesn't work window.location.href = "/logout";
             }
-          },
+          }
         );
       });
     }
@@ -148,7 +148,7 @@ class App extends React.Component {
       },
       (error) => {
         console.warn(error);
-      },
+      }
     );
   }
   componentDidMount() {
