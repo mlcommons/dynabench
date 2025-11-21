14c2332267a22b3ab3e37b5232cdadac177c6270
Sara Hincapié M <43832784+shincap8@users.noreply.github.com>
2025-11-21 09:21:20 -0500

    Update backend/app/domain/services/base/score.py
    Co-authored-by: Rafael Mosquera <rafael.mosquera@factored.ai>

--- backend/app/domain/services/base/score.py
+++ backend/app/domain/services/base/score.py
@@ -345,7 +345,7 @@ class ScoreService:
             )
         return converted_data
 
-    def get_maximun_principal_score_by_task(self, task_id: int) -> float:
+    def get_maximum_principal_score_by_task(self, task_id: int) -> float:
         yaml_file = self.task_repository.get_config_file_by_task_id(task_id)[0]
         if not yaml_file:
             return {"perf": 0.00}
